a15ba33ae117939d5d1981a83f4e1146b0aaaa8d
A00302457 <144638631+A00302457@users.noreply.github.com>
2024-04-17 21:11:47 +0000

    created one more method calculateTips.

--- Split_Bill_Classlib/CalculateTip.cs
+++ Split_Bill_Classlib/CalculateTip.cs
@@ -4,5 +4,20 @@ namespace Split_Bill_Classlib;
 
 public class CalculateTip
 {
+    public Dictionary<string, decimal> calculateTips(Dictionary<string, decimal> totalamount, float tips)
+    {
+        Dictionary<string,decimal> totaltip = new Dictionary<string, decimal>();
+        decimal totalbill = 0;
+        foreach(var i in totalamount)
+        {
+            totalbill += i.Value;
+        }
+        foreach(var i in totalamount)
+        {
+            decimal tip = i.Value * (decimal)(tips / 100f);
+            totaltip[i.Key] = tip;
+        }
+        return totaltip;
+    }
     
 }
